6803ba81b859afb7df3352308135e25b7f8f1eee
runegan <runegan@users.noreply.github.com>
2025-07-04 14:49:27 +0200

    Fix: Update AYON_LAST_WORKFILE environment variable to use local workfile path

--- client/ayon_sitesync/launch_hooks/pre_copy_last_published_workfile.py
+++ client/ayon_sitesync/launch_hooks/pre_copy_last_published_workfile.py
@@ -190,7 +190,7 @@ class CopyLastPublishedWorkfile(PreLaunchHook):
         # Keep source filepath for further path conformation
         self.data["source_filepath"] = last_published_workfile_path
 
-        self.data["env"]["AYON_LAST_WORKFILE"] = last_published_workfile_path
+        self.data["env"]["AYON_LAST_WORKFILE"] = local_workfile_path
 
     def _get_last_published_workfile_representation(self,
         project_name, folder_id, task_id, workfile_extensions
--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 """Package declaring addon version."""
 name = "sitesync"
 title = "SiteSync"
-version = "1.2.4+dev"
+version = "1.2.4+dev3"
 client_dir = "ayon_sitesync"
 
 ayon_required_addons = {
